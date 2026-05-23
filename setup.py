0266c97c9986c46b71a4b9008fd7a6fb8266231d
Hritik827 <78196827+Hritik827@users.noreply.github.com>
2026-05-23 02:35:34 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,8 +25,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name = 'mlproject_regression',
     version= '0.0.1',
-    author='Utkarsh Gaikwad',
-    author_email='gaikwadujg@gmail.com',
+    author='Hritik Patil',
+    author_email='hritikvivekpatil@gmail.com',
     packages = find_packages(),
     install_requires = get_requirements('requirements.txt')
 )
